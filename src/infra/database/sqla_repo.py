312d0bb5031e5dff81bcf2c3fe40b17481a23349
Daniil325 <potekhindaniil04@gmail.com>
2025-03-10 01:23:21 +0700

    feat: app sqla repo

---
+++ src/infra/database/sqla_repo.py
@@ -0,0 +1,41 @@
+from typing import Any, TypeVar
+from uuid import UUID, uuid4
+
+from sqlalchemy import delete, select, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+T = TypeVar("T")
+
+
+class SqlHelper[T]:
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    @staticmethod
+    def new_id() -> UUID:
+        # TODO use uuid7
+        return uuid4()
+
+    async def get_all(self) -> list[T]:
+        stmt = select(T)
+        return (await self.session.execute(stmt)).scalars()
+
+    async def get(self, id: UUID) -> T | None:
+        stmt = select(T).where(T.id == id)
+        return (await self.session.execute(stmt)).scalar_one_or_none()
+
+    async def create(self, item: T) -> None:
+        self.session.add(item)
+        await self.session.commit()
+        await self.session.refresh(item)
+
+    async def update(self, changes: dict[str, Any], id: UUID) -> None:
+        stmt = update(T).where(T.id == id).values(**changes)
+        await self.session.execute(stmt)
+        await self.session.commit()
+
+    async def delete(self, id: UUID) -> None:
+        stmt = delete(T).where(T.id == id)
+        await self.session.execute(stmt)
+        await self.session.commit()
