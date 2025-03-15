48b84f75eb2f133ab5588593deb097c5124e4b81
Ermakov Vladimir <goodstory152004@gmail.com>
2025-03-15 17:44:42 +0700

    добавление генерирующейся ошибки при интеграции с api

--- src/infra/integrations/api_baseclass.py
+++ src/infra/integrations/api_baseclass.py
@@ -13,9 +13,9 @@ class ApiUse(ABC):
                 r.raise_for_status()
                 return r.json()
             except httpx.RequestError as exc:
-                print(f"An error occurred while requesting {exc.request.url!r}.")
+                raise ValueError(f"An error occurred while requesting {exc.request.url!r}.")
             except httpx.HTTPStatusError as exc:
-                print(f"Error response {exc.response.status_code} while requesting {exc.request.url!r}.")
+                raise ValueError(f"Error response {exc.response.status_code} while requesting {exc.request.url!r}.")
 
     @abstractmethod
     async def parse(self):
